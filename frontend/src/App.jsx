58be9a28916ce2405613971e8e8869c4c08dbae2
Shivanand Gheji <shivagheji@gmail.com>
2025-09-06 20:36:33 +0530

    feat: Setup frontend with Vite, React, and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      {/* <h1 className=" border-input shadow-lg">shivanand gheji</h1> */}
+      <div className="prose">
+        <h1>Welcome</h1>
+        <h2 className=" text-chart-5">Welcome</h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
