fa6e58ded3bdbc551864fe3d08a1a9dbe5b6d879
Shivanand Gheji <shivagheji@gmail.com>
2025-09-07 13:59:41 +0530

    perform minor updates and miscellaneous tasks

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,27 +7,29 @@ import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import authRoutes from "./routes/auth.routes.js";
 import blogRoutes from "./routes/blog.routes.js";
-import { FRONTEND_ORIGIN } from "./utils/env.js";
-import {
-  ensureCsrfCookie,
-  csrfProtect,
-} from "./middlewares/csrf.middleware.js";
+// import { FRONTEND_ORIGIN } from "./utils/env.js";
+// import {
+//   ensureCsrfCookie,
+//   csrfProtect,
+// } from "./middlewares/csrf.middleware.js";
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: FRONTEND_ORIGIN,
-    credentials: true,
-  })
-);
-app.use(ensureCsrfCookie);
-app.use(csrfProtect);
+// app.use(
+//   cors({
+//     // origin: FRONTEND_ORIGIN,
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+// app.use(ensureCsrfCookie);
+// app.use(csrfProtect);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5500/api"; // change if your backend base differs
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5500/api";
 
 const api = axios.create({
   baseURL: API_BASE,
