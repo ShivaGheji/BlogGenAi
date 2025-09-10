6975b44ef1e209daa4ba8ec68906d400ba2be900
Shivanand Gheji <shivagheji@gmail.com>
2025-09-11 01:32:51 +0530

    feat(api): Add CSRF endpoint

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,7 +2,13 @@ import { Router } from "express";
 import { signUp, signIn, me, signOut } from "../controllers/auth.controller.js";
 import { requireAuth } from "../middlewares/auth.middleware.js";
 
+import { ensureCsrfCookie } from "../middlewares/csrf.middleware.js";
+
 const authRouter = Router();
+
+authRouter.get("/csrf", ensureCsrfCookie, (req, res) => {
+  res.json({ message: "CSRF cookie set" });
+});
 
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
