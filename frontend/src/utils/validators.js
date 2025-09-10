dfa70430c48d004969f8048f31cd298c49b3667e
Shivanand Gheji <shivagheji@gmail.com>
2025-09-10 22:59:03 +0530

    feat(validator): add zod for frontend validation

--- frontend/src/utils/validators.js
+++ frontend/src/utils/validators.js
@@ -15,5 +15,15 @@ export const generateSchema = z.object({
   prompt: z.string().min(5, "Prompt should be at least 5 chars"),
   tone: z.string().min(1, "Select tone"),
   targetAudience: z.string().min(1, "Target audience required"),
-  // keywords: z.string().optional(),
+  keywords: z
+    .string()
+    .transform((val) => {
+      return val
+        .split(/[, ]+/)
+        .map((item) => item.trim())
+        .filter((item) => item.length > 0);
+    })
+    .pipe(z.array(z.string()))
+    .optional()
+    .default([]),
 });
